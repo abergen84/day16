ba8c9de3535299c473c8d34afb3a884aa6752e9f
Adam Bergen <bergen@gmail.com>
2014-10-14 15:29:29 -0500

    testing to gh-pages

--- hw16.js
+++ hw16.js
@@ -1,16 +1,35 @@
 var githubUsername = "abergen84";
-var url = "https://api.github.com/users/"+abergen84;
+var url = "https://api.github.com/users/" + githubUsername;
 $.get(url).then(function(data){
     drawProfile(data);
 })
+
 function drawProfile(data) {
     document.body.innerHTML = [
-        '<h1>',
+
+        
+        '<img src=',
+        data.avatar_url,
+        '/>',
+
+        '<h1> Name:',
         data.name,
         '</h1>',
-        '<h2> blog:',
+
+        '<h2> Blog:',
         data.blog,
-        '</h2>'
+        '</h2>',
+
+        '<h3> Place of Bidness:',
+        data.location,
+        '</h3>',
+
+        '<h3> Email:',
+        data.email,
+        '</h3>'
+
+
+
     ].join('')
 }
 
