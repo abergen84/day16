e0e5e966e551b480ef9ff8a619f29edbc0b207e1
Adam Bergen <bergen@gmail.com>
2014-10-14 00:26:45 -0500

    blah

---
+++ hw16.js
@@ -0,0 +1,15 @@
+var githubUsername = "abergen84";
+var url = "https://api.github.com/users/"+abergen84;
+$.get(url).then(function(data){
+    drawProfile(data);
+})
+function drawProfile(data) {
+    document.body.innerHTML = [
+        '<h1>',
+        data.name,
+        '</h1>',
+        '<h2> blog:',
+        data.blog,
+        '</h2>'
+    ].join('')
+}
